9c5728937f55a8f2dc7aeaf86d0102897a3bdc57
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-07 00:07:17 -0300

    se cambiaron los estilos del proyecto

--- js/app.js
+++ js/app.js
@@ -1,51 +1,60 @@
-const fechaHoy = document.querySelector('#fecha');
-const tiempo = document.getElementById('tiempo');
-const segundosActual = document.getElementById('segundos');
-const amPm = document.getElementById('amPm');
+const fechaHoy = document.querySelector("#fecha");
+const tiempo = document.getElementById("tiempo");
+const segundosActual = document.getElementById("segundos");
+const amPm = document.getElementById("amPm");
 
-function reloj(){
+function reloj() {
+  const fecha = new Date();
+  const diaSemana = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+  ];
+  const mesesAnio = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
 
-const fecha = new Date();
-const diaSemana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
-const mesesAnio = [
-    "Enero", "Febrero", "Marzo", "Abril",
-    "Mayo", "Junio", "Julio", "Agosto",
-    "Septiembre", "Octubre", "Noviembre", "Diciembre"
-];
+  const dia = diaSemana[fecha.getDay()];
+  const mes = mesesAnio[fecha.getMonth()];
 
-const dia = diaSemana[fecha.getDay()];
-const mes = mesesAnio[fecha.getMonth()]
+  fechaHoy.innerHTML = `${dia} ${fecha.getDate()} de ${mes} de ${fecha.getFullYear()}`;
 
-fechaHoy.innerHTML = (`${dia} ${fecha.getDate()} de ${mes} de ${fecha.getFullYear()}`);
+  const horas = fecha.getHours();
+  const minutos = fecha.getMinutes();
+  const segundo = fecha.getSeconds();
 
-const horas = fecha.getHours();
-const minutos = fecha.getMinutes();
-const segundo = fecha.getSeconds();
-
-tiempo.innerHTML =
-  formatoTiempo(horas) + ':' + formatoTiempo(minutos) + ':'
+  tiempo.innerHTML = formatoTiempo(horas) + ":" + formatoTiempo(minutos) + ":";
 
   segundosActual.innerHTML = formatoTiempo(segundo);
 
   formatoAmPm(horas);
-
 }
 
-
-function formatoAmPm(hora){
-    if (hora < 12){
-        amPm.innerHTML = 'AM'
-    }else{
-        amPm.innerHTML = 'PM'
-    }
+function formatoAmPm(hora) {
+  if (hora < 12) {
+    amPm.innerHTML = "AM";
+  } else {
+    amPm.innerHTML = "PM";
+  }
 }
 
 function formatoTiempo(time) {
-    return time < 10 ? '0' + time : time;
+  return time < 10 ? "0" + time : time;
 }
-
-
-
 reloj();
-
 setInterval(reloj, 1000);
